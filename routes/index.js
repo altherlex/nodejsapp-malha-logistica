6b3f97788291b7f050e301ed331b069cb9fd5c1f
Altherlex <para.alves@gmail.com>
2015-05-24 20:01:38 -0300

    TODO: db connection using sequelize

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+//var cheerio = require('cheerio');
+var $ = require('cheerio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +11,23 @@ router.get('/', function(req, res, next) {
 
 router.get('/figure', function(req, res, next) {
   res.send('figure');
+});
+
+router.get('/paths/create_massive', function(req, res, next) {
+/*
+  paths = []
+  params[:massive_paths].split(/\r\n/).each do |line|
+    paths << Path.massive_new(*line.split(','))
+  end
+  Path.import(paths)
+  redirect_to({action: :index }, notice: "Paths was successfully created.")  
+*/     
+  var paths = new Array;
+  var p = req.query.massive_paths.split(/\r\n/);
+  $(p).each(function(i,v){
+    console.log(v.split(','));
+  });
+  res.redirect('/');
 });
 
 
