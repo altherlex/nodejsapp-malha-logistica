64f372684aaee033300de5dc7dad06a09b59a3cb
Altherlex <para.alves@gmail.com>
2015-05-24 19:06:00 -0300

    ajuste layout

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var info_pattern = "Sampa, A, B, 10\r\nSampa, B, D, 15\r\nSampa, A, C, 20\r\nSampa, C, D, 30\r\nSampa, B, E, 50\r\nSampa, D, E, 30"
-  res.render('index');
+  res.render('index', {info_pattern:info_pattern});
 });
 
 router.get('/figure', function(req, res, next) {
